79860430f2ff4f083cbc9a65086359b1070ed9b6
svolain <savoville@gmail.com>
2024-12-17 16:46:37 +0200

    frontend for creating and deleting notes

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StyleIcon from '@mui/icons-material/Style';
+
+function Header() {
+  return (
+    <header>
+      <h1><StyleIcon />Keep</h1>
+    </header>
+  );
+}
+
+export default Header;
