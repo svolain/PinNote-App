f60e39e402ab40b760a1ead7b63c8da19fcca230
svolain <savoville@gmail.com>
2024-12-21 17:04:00 +0200

    added ssl to pool.js

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,21 +1,16 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-
-
 const pool = new Pool({
-    /*for local development
+    /* Local development
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,*/
 
-    //for deployment
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false, // This is required for many cloud-hosted Postgres instances.
-    }
+    ssl: { rejectUnauthorized: false },
   });
   
   module.exports = pool;
