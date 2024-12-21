e9186f81b954c6d543c79dc30bd66cb0b6c62ec6
svolain <savoville@gmail.com>
2024-12-21 14:04:48 +0200

    added server.js port for deployment

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 
 
 const pool = new Pool({
-    //for local development
-    //user: process.env.DB_USER,
-    //host: process.env.DB_HOST,
-    //database: process.env.DB_NAME,
-    //password: process.env.DB_PASS,
-    //port: process.env.DB_PORT,
+    /*for local development
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,*/
 
     //for deployment
     connectionString: process.env.DATABASE_URL,
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 const frontendPath = path.join(__dirname, "./frontend/dist");
 app.use(express.static(frontendPath));
 
@@ -17,6 +19,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("Server running on http://localhost:5000");
 });
