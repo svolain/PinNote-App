cd39fbdb5088b5e82a0650765b4cabbe3e29e793
svolain <savoville@gmail.com>
2024-12-21 14:47:12 +0200

    new try with env port

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,6 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 
-const PORT = process.env.PORT || 5000;
-
 const frontendPath = path.join(__dirname, "./frontend/dist");
 app.use(express.static(frontendPath));
 
@@ -19,6 +17,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server running on http://localhost:5000");
+  console.log(`Server running on port ${PORT}`);
 });
